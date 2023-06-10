05fb9ece8c15334f9a256ff2682ed9ade6c1e36d
Li Ming <Goby.hl@gmail.com>
2023-06-10 22:35:12 +0800

    Update BingWallpaper.sh

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -10,13 +10,16 @@ fi
 findResult=$(find $localDir -regex $filenameRegex)
 
 if [ ! -n "$findResult" ]; then
-    baseUrl="cn.bing.com"
+    baseUrl="bing.com"
     html=$(curl -L -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.51" $baseUrl)
-    imgurl=$(echo "$html" | grep 'preload' | grep -oE 'https://[a-zA-Z0-9./?=_-]*_1920x1080.jpg' | head -n 1)
+    echo $html
+    imgurl=$(echo "$html" | grep 'preload' | grep -oE 'https://[a-zA-Z0-9./?=_-]*_1920x1080.webp' | head -n 1)
     imgurl=$(echo $imgurl | sed 's/1920x1080/UHD/') 
-    echo "img: $imgurl"
+    imgurl="${imgurl/%.webp/.jpg}"
+    echo "imgurl: $imgurl"
     filename=$(echo $imgurl | sed -n 's/.*id=OHR\.\([^&]*\).*/\1/p')
-    echo $filename
+    filename="${filename/%.webp/.jpg}"
+    echo "filename: $filename"
     localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
     curl --output $localpath -H 'Cache-Control: no-cache' $imgurl
 
